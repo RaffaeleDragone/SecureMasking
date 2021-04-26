2f1b930e2d8146cc37a52df818486d846bd080b0
Raffaele Dragone <dragone.raffaele1@gmail.com>
2021-04-26 10:55:17 +0200

    UtilCompression

--- src/it/unisa/compressionedati/utils/UtilCompression.java
+++ src/it/unisa/compressionedati/utils/UtilCompression.java
@@ -1,19 +1,29 @@
 package it.unisa.compressionedati.utils;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
 import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
 import javax.imageio.ImageWriteParam;
 import javax.imageio.ImageWriter;
 import javax.imageio.stream.ImageOutputStream;
 import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Base64;
 import java.util.zip.DeflaterOutputStream;
 import java.util.zip.InflaterOutputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.bind.DatatypeConverter;
 
 public class UtilCompression {
 
@@ -83,5 +93,73 @@ public class UtilCompression {
         return jpegData;
     }
 
+    public static String write_content(String input, String name_file){
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] digest = md.digest(input.getBytes());
+            String myHash = DatatypeConverter.printHexBinary(digest).toUpperCase();
+
+            JSONObject jsObj = new JSONObject();
+            jsObj.put(myHash,input);
+
+            String to_write = encrypterText(jsObj.toJSONString());
+
+            //Write JSON file
+            try (FileWriter file = new FileWriter(name_file)) {
+                //We can write any JSONArray or JSONObject instance to the file
+                file.write(to_write);
+                file.flush();
+                return myHash;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+        } catch (NoSuchAlgorithmException ex) {
+            Logger.getLogger(UtilCompression.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    public static String read_content(String name_file,String key) throws IOException, ParseException {
+        //JSON parser object to parse read file
+
+
+        String content_file = new String ( Files.readAllBytes( Paths.get(name_file) ) );
+        String to_p = decrypterText(content_file);
+
+        JSONParser jsonParser = new JSONParser();
+        JSONObject jsObj = (JSONObject) jsonParser.parse(to_p);
+        String parse = jsObj.get(key)!=null ? jsObj.get(key).toString() : null;
+
+        return parse;
+
+    }
+
+    public static String encrypterText(String value) {
+        try {
+            SecretKeySpec key = new SecretKeySpec("My_Secret_Key".getBytes(), "Blowfish");
+            Cipher cipher = Cipher.getInstance("Blowfish");
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+            byte[] plainTxtBytes = value.getBytes("UTF-8");
+            byte[] encBytes = cipher.doFinal(plainTxtBytes);
+            return new sun.misc.BASE64Encoder().encode(encBytes);
+        } catch (Exception ex) {
+            return value;
+        }
+    }
+
+    public static String decrypterText(String value) {
+        try {
+            SecretKeySpec key = new SecretKeySpec("My_Secret_Key".getBytes(), "Blowfish");
+            Cipher cipher = Cipher.getInstance("Blowfish");
+            cipher.init(Cipher.DECRYPT_MODE, key);
+            byte[] encBytes = new sun.misc.BASE64Decoder().decodeBuffer(value);
+            byte[] plainTxtBytes = cipher.doFinal(encBytes);
+            return new String(plainTxtBytes);
+        } catch (Exception ex) {
+            return value;
+        }
+    }
 
 }
